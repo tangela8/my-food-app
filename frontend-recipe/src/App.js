62034b8f991f9d77eeddc29c94fe54e1c86801ae
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-06-03 00:18:32 -0400

    created search bar and api

--- frontend-recipe/src/App.js
+++ frontend-recipe/src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from "axios";
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const YOUR_APP_ID = "6d63820c";
+  const YOUR_APP_KEY = "95058aa743f9cc356f1c0c9d75755afec";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await Axios.get(url);
+
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+    }
+  };
+
+  const onChange = e => setQuery(e.target.value);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    getData();
+  };
   return (
     <div className="App">
       <h1>Recipe Search</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
 }
